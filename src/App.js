26efd82fc3536ad7adbfd7d93a80071c41952e41
Oleksandr Husiev <ohusiev@icloud.com>
2021-11-18 14:53:44 +0100

    monster array render

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import {Component} from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      string: 'Hello Alex'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 1
+        },
+        {
+          name: 'Dracula',
+          id: 2
+        },
+        {
+          name: 'Zombie',
+          id: 3
+        }
+      ]
     }
   }
 
   render() {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo'/>
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({string: 'hello Nata'})}>Change text</button>
-        </header>
+        {
+          this.state.monsters.map(monster =>
+            <h1 key={monster.id}>{monster.name}</h1>)
+        }
       </div>
     )
   }
