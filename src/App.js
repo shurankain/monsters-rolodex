9d17e9f861c980ad7ed04b738d4947e785c6534b
Oleksandr Husiev <ohusiev@icloud.com>
2022-01-05 00:19:36 +0100

    card-list component added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {Component} from 'react'
 import './App.css'
+import {CardList} from './components/card-list/card-list.component'
 
 class App extends Component {
   constructor() {
@@ -12,16 +13,18 @@ class App extends Component {
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(res => res.json())
-      .then(usersArr => this.setState({ monsters: usersArr}))
+      .then(usersArr => this.setState({monsters: usersArr}))
   }
 
   render() {
     return (
       <div className='App'>
-        {
-          this.state.monsters.map(monster =>
-            <h1 key={monster.id}>{monster.name}</h1>)
-        }
+        <CardList>
+          {
+            this.state.monsters.map(monster =>
+              <h1 key={monster.id}>{monster.name}</h1>)
+          }
+        </CardList>
       </div>
     )
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,7 @@
+import './card-list.styles.css'
+
+export const CardList = props => {
+  return <div className='card-list'>
+    {props.children}
+  </div>
+}
