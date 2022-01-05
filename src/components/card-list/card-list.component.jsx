cfcf95e1a08ad54af88e9155abf7fa04cb59e9ae
Oleksandr Husiev <ohusiev@icloud.com>
2022-01-05 15:03:50 +0100

    card component added

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,7 +1,11 @@
 import './card-list.styles.css'
 
+import { Card} from '../card/card.component'
+
 export const CardList = props => {
   return <div className='card-list'>
-    {props.children}
+    {
+      props.monsters.map(monster => <Card key={monster.id} monster={monster}/>)
+    }
   </div>
 }
