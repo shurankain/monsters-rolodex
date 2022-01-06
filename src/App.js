a2a654bae10adee5f2044fc1936de9c00f96002b
Oleksandr Husiev <ohusiev@icloud.com>
2022-01-06 14:13:26 +0100

    search logic

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -17,9 +18,16 @@ class App extends Component {
   }
 
   render() {
+    const {monsters, searchField} = this.state
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()))
+
     return (
       <div className='App'>
-        <CardList monsters={this.state.monsters}/>
+        <input type={'search'}
+               placeholder={'search monsters'}
+               onChange={e => this.setState({searchField: e.target.value})}/>
+        <CardList monsters={filteredMonsters}/>
       </div>
     )
   }
