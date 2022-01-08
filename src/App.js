c5a6d03222da104ebbc6311455990f12f3a96617
Oleksandr Husiev <ohusiev@icloud.com>
2022-01-09 00:44:06 +0100

    search component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Component} from 'react'
 import './App.css'
 import {CardList} from './components/card-list/card-list.component'
+import {SearchBox} from './components/search-box/search-box.component'
 
 class App extends Component {
   constructor() {
@@ -24,9 +25,10 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <input type={'search'}
-               placeholder={'search monsters'}
-               onChange={e => this.setState({searchField: e.target.value})}/>
+        <SearchBox
+          placeholder={'search monsters'}
+          handleChange={e => this.setState({searchField: e.target.value})}
+        />
         <CardList monsters={filteredMonsters}/>
       </div>
     )
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,7 +2,7 @@ import './card-list.styles.css'
 
 import { Card} from '../card/card.component'
 
-export const CardList = props => {
+export const CardList = props => { 
   return <div className='card-list'>
     {
       props.monsters.map(monster => <Card key={monster.id} monster={monster}/>)
